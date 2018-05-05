5d8f286b81a0b11ce60c93600c681ea3d225ef8b
Np <fearhev152400@gmail.com>
2018-05-05 15:19:35 +0800

    Refactored the code, now with backgrounds rendered

--- game.cpp
+++ game.cpp
@@ -1,82 +1,112 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
 
 using namespace std;
 
-/*
- * Window class
- * Class including everything about handling events of game window.
- * A window object contains height and width (px), window name,
- * and a boolean value for indicating the window is closed or not.
- * Window is instantiated using a SDL_Window object.
- * */
-class Window {
+class MainGame{
 private:
-    int wheight = 800;
-    int wwidth = 600;
-    string wTitle = "Zombiroid";
-    bool wClosed = false;
+    int wheight;
+    int wwidth;
+    string wTitle;
+    bool gRun;
 
-    SDL_Window *window = nullptr;
-
+    SDL_Window *window;
+    SDL_Renderer *mRenderer;
+    SDL_Texture *bkgnd, *pSprite, *eSprite;
+    SDL_Surface *texTemp;
 
 public:
-    Window(const std::string &wt, int width, int height) {
-        wTitle = wt, wwidth = width, wheight = height;
-        if (!wInit()) {
-            wClosed = true;
-        }
+    MainGame() {};
+    ~MainGame() {};
 
-    };
-    ~Window() {
+
+    void events() {
+        SDL_Event gameEvent;
+        SDL_PollEvent(&gameEvent);
+        switch (gameEvent.type) {
+            case SDL_QUIT:
+                gRun = false;
+                break;
+            default:
+                break;
+        }
+    }
+
+    void gameInit(string title, int height, int width) {
+        wTitle = title;
+        wwidth = width;
+        wheight = height;
+        if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+            cout << "SDL Initialized." << endl;
+
+            window = SDL_CreateWindow(wTitle.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,wwidth,wheight,0);
+            if (window) { cout << "Window Initialized and created." << endl; }
+
+            mRenderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_PRESENTVSYNC);
+            if (mRenderer) {
+                cout << "Renderer Initialized." << endl;
+                SDL_SetRenderDrawColor(mRenderer,128,128,128,255);
+
+            }
+
+            texTemp = IMG_Load("assets/bg.jpg");
+            if (texTemp!=NULL) {
+                bkgnd = SDL_CreateTextureFromSurface(mRenderer,texTemp);
+                SDL_FreeSurface(texTemp);
+            } else {
+                SDL_SetRenderDrawColor(mRenderer,242,164,75,255);
+                cerr << IMG_GetError() << endl;
+            }
+
+
+            gRun = true;
+        } else { gRun = false; cerr << "Something is not right here." << endl; }
+
+
+    }
+
+    bool runIndicator() { return gRun; }
+
+    void gameRender() {
+        SDL_RenderClear(mRenderer);
+        if (bkgnd) { SDL_RenderCopy(mRenderer,bkgnd,NULL,NULL); }
+        SDL_RenderPresent(mRenderer);
+    }
+
+    void spriteUpdater() {}
+
+    void gameExit() {
+        SDL_DestroyRenderer(mRenderer);
         SDL_DestroyWindow(window);
         SDL_Quit();
-    };
-
-    bool wInit() {
-        if (SDL_Init(SDL_INIT_VIDEO)!=0) {
-            cerr<<"SDL Initialization failed.\n";
-            return -1;
-        }
-
-        window = SDL_CreateWindow(
-                wTitle.c_str(),
-                SDL_WINDOWPOS_CENTERED,  // Constant SDL_WINDOWPOS_CENTERED indicates starting points
-                SDL_WINDOWPOS_CENTERED,  // which always make the window at center of display.
-                wwidth, wheight,0
-        );
-        if (window == nullptr) {
-            cerr<<"Window Initialization failed.\n";
-            return -1;
-        }
-        return true;
+        cout << "Game process ended." << endl;
     }
 
-    inline bool isClosed() const{return wClosed;}
-
-    void wEventsListener() {
-        SDL_Event wEvent;
-
-        if (SDL_PollEvent(&wEvent)) {
-            switch (wEvent.type) {
-                case SDL_QUIT:
-                    wClosed = true;
-                    break;
-
-                default:
-                    break;
-            }
-        }
-    }
 };
 
-int main() {
 
-    Window gameWindow("Zombiroid - Asteroid with zombies", 800, 600);
 
-    while(!gameWindow.isClosed()) {
-        gameWindow.wEventsListener();
+int main () {
+    MainGame *game = nullptr;
+    string gTitle = "Zombiroid";
+    int height = 800;
+    int width = 800;
+    game = new MainGame();
+
+    game->gameInit(gTitle,height,width);
+
+    while (game->runIndicator()) {
+
+
+        game->events();
+        game->spriteUpdater();
+        game->gameRender();
+
     }
+
+    game->gameExit();
+
 
     return 0;
 }
